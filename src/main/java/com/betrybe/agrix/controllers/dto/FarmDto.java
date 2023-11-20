fd9e67ea27b2581b80de47326c0f23315705814e
Ruan-Portella <ruanmorales29@gmail.com>
2023-11-20 11:58:43 -0300

    Fix: Arruma problema de retorno dos farms

--- src/main/java/com/betrybe/agrix/controllers/dto/FarmDto.java
+++ src/main/java/com/betrybe/agrix/controllers/dto/FarmDto.java
@@ -6,7 +6,17 @@ import com.betrybe.agrix.models.entities.Farm;
  * FarmDTO.
  */
 public record FarmDto(Long id, String name, double size) {
+
   public Farm toFarm() {
     return new Farm(id, name, size);
+  }
+
+  /**
+ * FarmDTO.
+ */
+  public static record Creation(String name, double size) {
+    public Farm toFarm() {
+      return new Farm(name, size);
+    }
   }
 }
--- src/main/java/com/betrybe/agrix/models/entities/Farm.java
+++ src/main/java/com/betrybe/agrix/models/entities/Farm.java
@@ -1,13 +1,13 @@
 package com.betrybe.agrix.models.entities;
 
-import java.util.List;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.List;
 
 /**
  * Class Farm.
@@ -20,22 +20,34 @@ public class Farm {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
-  private double size;
-
+  private Double size;
+  
+  @JsonIgnore
   @OneToMany(mappedBy = "farm")
   private List<Crop> crops;
+
+  public Farm() {}
 
   /**
  * Metodo contrutor.
 */
-
-  public Farm() {
-  }
-
   public Farm(Long id, String name, Double size) {
     this.id = id;
     this.name = name;
     this.size = size;
+  }
+
+  public Farm(String name, double size) {
+    this.setName(name);
+    this.setSize(size);
+  }
+  
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
   }
 
   public String getName() {
@@ -50,22 +62,14 @@ public class Farm {
     return size;
   }
 
-  public void setSize(double size) {
+  public void setSize(Double size) {
     this.size = size;
   }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(long id) {
-    this.id = id;
-  }
-
+  
   public List<Crop> getCrops() {
     return crops;
   }
-
+  
   public void setCrops(List<Crop> crops) {
     this.crops = crops;
   }
